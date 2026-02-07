f047d95b30ea2f04e2f406bddb5d177273e6093f
YihalemM <yihalemmulat45@gmail.com>
2026-02-07 17:29:32 +0300

    Add minimum deposit limit, withdrawal time-lock, and per-user transaction history

--- StudentSavingWallet/StudentSavingWallet.sol
+++ StudentSavingWallet/StudentSavingWallet.sol
@@ -2,8 +2,12 @@
 pragma solidity ^0.8.0;
 
 contract StudentSavingWallet {
-    // Store user balances
+    // State variables
     mapping(address => uint256) private balances;
+    mapping(address => Transaction[]) private userTransactions;
+    mapping(address => uint256) private lastDepositTime;
+    uint256 minDeposit = 0.01 ether; // minimum depoait
+    uint256 public lockPeriod = 2 minutes; // withdrawal lock for testing
 
     // Transaction structure
     struct Transaction {
@@ -13,34 +17,39 @@ contract StudentSavingWallet {
         uint256 timestamp; // Time of the transaction
     }
 
-    // Array to store all transactions
-    Transaction[] private transactions;
     //Events
     event Deposit(address indexed user, uint256 amount, uint256 timestamp);
     event Withdrawal(address indexed user, uint256 amount, uint256 timestamp);
 
+    // --------FUNCTIONS--------------------
+
     function deposit() public payable {
-        require(msg.value > 0, "Deposit amount must be greater than zero");
-
-        // Add ETH to user's balance
-        balances[msg.sender] += msg.value;
-
-        // Record deposit transaction
-        transactions.push(
-            Transaction({
-                user: msg.sender,
-                amount: msg.value,
-                isDeposit: true,
-                timestamp: block.timestamp
-            })
+        require(
+            msg.value >= minDeposit,
+            "Deposit amount must be at least 0.01 ETH"
         );
+
+        // Update user balance
+        balances[msg.sender] += msg.value;
+        // Update last timestamp of deposit for the user
+        lastDepositTime[msg.sender] = block.timestamp;
+        // Record deposit transaction
+        userTransactions[msg.sender].push(
+            Transaction(msg.sender, msg.value, true, block.timestamp)
+        );
+
         emit Deposit(msg.sender, msg.value, block.timestamp);
     }
 
     function withdraw(uint256 amount) public {
+        // ensure user has enough balance and funds are not locked
         require(balances[msg.sender] >= amount, "Insufficient balance");
+        require(
+            block.timestamp >= lastDepositTime[msg.sender] + lockPeriod,
+            "Funds are locked. for 2 minutes after deposit"
+        );
 
-        // Subtract from balance
+        // Deduct balance
         balances[msg.sender] -= amount;
 
         // Transfer ETH to user
@@ -48,13 +57,8 @@ contract StudentSavingWallet {
         require(success, "ETH transfer failed");
 
         // Record withdrawal transaction
-        transactions.push(
-            Transaction({
-                user: msg.sender,
-                amount: amount,
-                isDeposit: false,
-                timestamp: block.timestamp
-            })
+        userTransactions[msg.sender].push(
+            Transaction(msg.sender, amount, false, block.timestamp)
         );
         emit Withdrawal(msg.sender, amount, block.timestamp);
     }
@@ -70,6 +74,6 @@ contract StudentSavingWallet {
         view
         returns (Transaction[] memory)
     {
-        return transactions;
+        return userTransactions[msg.sender];
     }
 }
